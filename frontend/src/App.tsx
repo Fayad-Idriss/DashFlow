f28890f6158bc60855c5688dd97084997f74276e
Fayad-Idriss BACARI <fayad-idriss.bacari@outlook.com>
2024-09-24 21:27:20 +0200

    liaison front + back

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [msg, setMsg] = useState()
+  const handleClick = async () => {
+      const data = await window.fetch('api/youtube')
+      const json = await data.json()
+      console.log(json)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <button onClick={handleClick}>
+            dis bonjour 
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
