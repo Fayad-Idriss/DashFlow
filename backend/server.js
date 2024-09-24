bf8f79a9351fa4210845f31d26bc3d04eeb404de
Fayad-Idriss BACARI <fayad-idriss.bacari@outlook.com>
2024-09-24 19:52:19 +0200

    backend v2

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
-const express = require('express')
+const express = require('express');
+require('dotenv').config();
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 7000; 
+
+app.use(express.json())
+app.get('/api/youtube', (_, res) => {
+    res.send({
+        msg: 'hello les putes'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Le serveur est lancé sur le port : ${PORT}`)
+
+});
